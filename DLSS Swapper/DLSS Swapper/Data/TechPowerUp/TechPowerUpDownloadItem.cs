95dcb85365ca4a4fdc91f7ca5112642466dd466c
Brad <brad@4pi.com.au>
2021-08-07 10:55:10 +1000

    Added techpowerup downloads reference list

---
+++ DLSS Swapper/DLSS Swapper/Data/TechPowerUp/TechPowerUpDownloadItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DLSS_Swapper.Data.TechPowerUp
+{
+    record TechPowerUpDownloadItem
+    {
+        [JsonPropertyName("filename")]
+        public string FileName { get; }
+
+        [JsonPropertyName("md5_hash")]
+        public string MD5Hash { get; }
+    }
+}

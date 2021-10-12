849843909e604d6311e476adc4ba0d5ff54151c8
Brad Moore <brad@4pi.com.au>
2021-10-12 16:47:18 +1100

    Fixes issue of Steam games sometimes being duplicated
    Should fix #45

---
+++ DLSS Swapper/DLSS Swapper/Helpers/PathHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLSS_Swapper.Helpers
+{
+    internal class PathHelpers
+    {
+        /// <summary>
+        /// Tries to format path on disk so any and all paths will match after they have gone through this method.  
+        /// </summary>
+        /// <param name="path">Path on local disk</param>
+        /// <returns>Formatted path on disk</returns>
+        internal static string NormalizePath(string path)
+        {
+            // Via https://stackoverflow.com/a/21058152
+            //new Uri(path).LocalPath
+            return Path.GetFullPath(path).TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+        }
+    }
+}

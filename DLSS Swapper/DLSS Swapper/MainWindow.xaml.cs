8622479452ae07e7276e4b122e270cb2941d0708
Brad Moore <brad@4pi.com.au>
2022-02-19 16:55:17 +1100

    Removed beta tag from title heading to 1.0 release.

--- DLSS Swapper/DLSS Swapper/MainWindow.xaml.cs
+++ DLSS Swapper/DLSS Swapper/MainWindow.xaml.cs
@@ -42,7 +42,7 @@ namespace DLSS_Swapper
 
         public MainWindow()
         {
-            Title = "DLSS Swapper [beta]";
+            Title = "DLSS Swapper";
             this.InitializeComponent();
             NavigationView = MainNavigationView;
 

bc06d367e1e390b7b29b9ff87677b19a667b3b7f
Brad <brad@4pi.com.au>
2022-10-16 11:50:43 +1100

    Added extra error logging to xbox library.

--- DLSS Swapper/DLSS Swapper/Data/Xbox/XboxLibrary.cs
+++ DLSS Swapper/DLSS Swapper/Data/Xbox/XboxLibrary.cs
@@ -196,7 +196,7 @@ namespace DLSS_Swapper.Data.Xbox
                     }
                     catch (Exception err)
                     {
-
+                        Logger.Error(err.Message);
                     }
                 }
             }

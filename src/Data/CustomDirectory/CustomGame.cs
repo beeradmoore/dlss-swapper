58b41fd41b12435903a3369972dca29275a5f442
BmKo <60381441+BmKo@users.noreply.github.com>
2023-08-23 09:08:26 +1200

    Create CustomGame

---
+++ src/Data/CustomDirectory/CustomGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DLSS_Swapper.Data.CustomDirectory;
+
+public class CustomGame : Game
+{
+    public override string HeaderImage => String.Empty;
+
+    public CustomGame(String name, String path)
+    {
+        Title = name;
+        InstallPath = path;
+        
+        DetectDLSS();
+    }
+}

ef9fc373db598db7319272bf3680346ea0189edc
Brad <brad@4pi.com.au>
2024-10-26 14:13:07 +1100

    FIxed binding for when CoverImage is an empty string

---
+++ src/Converters/BitmapImageUriConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Data;
+
+namespace DLSS_Swapper.Converters
+{
+    internal class BitmapImageUriConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string stringValue)
+            {
+                if (string.IsNullOrEmpty(stringValue))
+                {
+                    return null;
+                }
+
+                return new Uri(stringValue);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

5b43fdf0f7813b9dcc7781374a8a960b3e497d0e
Brad <brad@4pi.com.au>
2024-11-02 15:34:47 +1100

    Attempt to diagnose file in use error

--- src/Extensions/FileVersionInfoExtensions.cs
+++ src/Extensions/FileVersionInfoExtensions.cs
@@ -14,7 +14,7 @@ namespace DLSS_Swapper.Extensions
         {
             try
             {
-                using (var fileStream = new FileStream(fileVersionInfo.FileName, FileMode.Open))
+                using (var fileStream = File.OpenRead(fileVersionInfo.FileName))
                 {
                     return fileStream.GetMD5Hash();
                 }
@@ -22,6 +22,7 @@ namespace DLSS_Swapper.Extensions
             catch (Exception err)
             {
                 Logger.Error($"{fileVersionInfo.FileName}, {err.Message}");
+                Debugger.Break();
             }
 
             return string.Empty;

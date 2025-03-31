996b01ae8f9f7ac4d4ba5726277a2a930cd3cd83
Kur3k <kur4k@proton.me>
2025-04-01 01:52:53 +0200

    Cleaning GameGridPage, settings name => Scan Drives.

--- src/Pages/GameGridPage.xaml.cs
+++ src/Pages/GameGridPage.xaml.cs
@@ -1,40 +1,11 @@
 using DLSS_Swapper.Data;
-using DLSS_Swapper.Data.EpicGamesStore;
-using DLSS_Swapper.Data.GOG;
-using DLSS_Swapper.Data.GitHub;
-using DLSS_Swapper.Data.Steam;
-using DLSS_Swapper.Data.UbisoftConnect;
-using DLSS_Swapper.Data.Xbox;
-using DLSS_Swapper.Interfaces;
 using DLSS_Swapper.UserControls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.Principal;
-using System.Text.Json;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Pickers;
 using Windows.System;
-using System.Text;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.CodeDom;
-using System.Collections.Concurrent;
 using AsyncAwaitBestPractices;
 using CommunityToolkit.WinUI;
 using System.Threading;
@@ -180,19 +151,6 @@ namespace DLSS_Swapper.Pages
                 FilterGames();
             });
             */
-        }
-
-
-
-        bool hasFirstLoaded = false;
-        void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-
-
-            //await LoadGamesAndDlls();
-            //await LoadGamesFromCacheAsync();
-            //UpdateGameLibraries();
-            //await LoadGames();
         }
 
         async void MainGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)

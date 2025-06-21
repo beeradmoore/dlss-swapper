9d42b4f5aa5e5db81aedfa2d35a999ed1d5f8052
Brad <brad@4pi.com.au>
2025-06-21 12:55:09 +1000

    Added streamline to acknowledgements

--- src/Pages/AcknowledgementsPageModel.cs
+++ src/Pages/AcknowledgementsPageModel.cs
@@ -35,6 +35,7 @@ public partial class AcknowledgementsPageModel : ObservableObject
         {
             "You",
             "DLSS",
+            "Streamline",
             "FidelityFX-SDK",
             "XeSS"
         };

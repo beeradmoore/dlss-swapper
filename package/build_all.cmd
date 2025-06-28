af662773caa83810054957b22ac17ad4af48114e
Brad <brad@4pi.com.au>
2025-06-28 12:33:36 +1000

    Made the buildall script update static_manifest by default

--- package/build_all.cmd
+++ package/build_all.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+REM Update the static_manifest.json to the latest version.
+REM We only do this in the build_all as to ensure both versions have THE EXACT same file.
+call ..\update_manifest.cmd || goto :error
+
 call build_Portable.cmd || goto :error
 call build_Installer.cmd || goto :error
 

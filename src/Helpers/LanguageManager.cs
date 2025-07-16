59a9db79d8899d25ca1993b377a16c92188b8afc
Abdulrahman ALOTHMAN <30497609+aboodgamer24@users.noreply.github.com>
2025-07-16 07:46:46 +0200

    Update LanguageManager.cs

--- src/Helpers/LanguageManager.cs
+++ src/Helpers/LanguageManager.cs
@@ -42,6 +42,7 @@ public class LanguageManager
     {
         // For now this is a hardcoded list. It would be nice to dynamically discover from resource class or something.
         return [
+            "ar-SY",
             "de-DE",
             "en-AU",
             "en-GB",
@@ -63,6 +64,7 @@ public class LanguageManager
         // For now this is a hardcoded list. It would be nice to dynamically discover from resource class or something.
         return languageKey switch
         {
+            "ar-SY" => "العربية", // Arabic
             "de-DE" => "Deutsch (Deutschland)", // German (Germany)
             "en-AU" => "English (Australia)",
             "en-GB" => "English (United Kingdom)",

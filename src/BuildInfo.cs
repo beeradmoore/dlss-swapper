bf7e8bbdd9c81ec8087f75fe910e41b70d5205a0
Brad <brad@4pi.com.au>
2025-08-02 12:10:09 +1000

    Create BuildInfo.cs

---
+++ src/BuildInfo.cs
@@ -0,0 +1,11 @@
+
+namespace DLSS_Swapper;
+
+internal static class BuildInfo
+{
+    public static string GitBranch { get; } = string.Empty;
+    public static string GitCommit { get; } = string.Empty;
+    public static string GitTag { get; } = string.Empty;
+    public static long BuildTimestamp { get; } = 1754100579;
+
+}

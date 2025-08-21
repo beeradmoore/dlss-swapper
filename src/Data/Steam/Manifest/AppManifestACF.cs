0cb59e87695eb92cd8c6ad9fdbab4c9f2f01664a
Brad <brad@4pi.com.au>
2025-08-21 19:39:45 +1000

    Bugfix/steam loading improvements (#691)
    * Added initial change to loading steam files from our own parser to using ValveKeyValue
    
    * Updated to using root level manifest
    
    * Bump version to v1.2.1.1

---
+++ src/Data/Steam/Manifest/AppManifestACF.cs
@@ -0,0 +1,9 @@
+namespace DLSS_Swapper.Data.Steam.Manifest;
+
+internal class AppManifestACF
+{
+    public string AppId { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string StateFlags { get; set; } = string.Empty;
+    public string InstallDir { get; set; } = string.Empty;
+}

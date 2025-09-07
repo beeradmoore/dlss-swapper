4d8b82a1cb3d173cdaa35fd02a7165416a30e07f
Brad <brad@4pi.com.au>
2025-09-07 11:05:19 +1000

    Added better handling of freshly installed Steam games (#736)
    * Added better handling of freshly installed Steam games
    
    * Changed Steam cover loading to use IStoreBrowseService first

--- src/Data/Steam/SteamGame.cs
+++ src/Data/Steam/SteamGame.cs
@@ -61,86 +61,97 @@ internal partial class SteamGame : Game
             return;            
         }
 
-        // If it doesn't exist, load from web.
-        var didDownload = await DownloadCoverAsync($"https://steamcdn-a.akamaihd.net/steam/apps/{PlatformId}/library_600x900_2x.jpg").ConfigureAwait(false);
-
-        // If we couldn't download the cover try getting it from the IStoreBrowseService.
+        // Try download via IStoreBrowseService first.
+        var didDownload = await DownloadCoverFromIStoreBrowseService();
         if (didDownload == false)
         {
-            try
+            // Try the old cover system?
+            didDownload = await DownloadCoverAsync($"https://steamcdn-a.akamaihd.net/steam/apps/{PlatformId}/library_600x900_2x.jpg").ConfigureAwait(false);
+
+            if (didDownload == false)
             {
-                var getItemsInput = new GetItemsInput();
-                getItemsInput.Ids.Add(new StoreItemId() { AppId = Int32.Parse(PlatformId, System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture) });
-                getItemsInput.DataRequest.IncludeAssets = true;
+                Logger.Error($"Tried to get Steam cover for {PlatformId} but was unable to get it from both old and new Steam CDNs.");
+            }
+        }
+    }
 
-                var jsonPayload = JsonSerializer.Serialize(getItemsInput);
-                var payloadUrlEncoded = HttpUtility.UrlEncode(jsonPayload);
+    async Task<bool> DownloadCoverFromIStoreBrowseService()
+    {
+        try
+        {
+            var getItemsInput = new GetItemsInput();
+            getItemsInput.Ids.Add(new StoreItemId() { AppId = Int32.Parse(PlatformId, System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.InvariantCulture) });
+            getItemsInput.DataRequest.IncludeAssets = true;
 
-                using (var steamApiResponse = await App.CurrentApp.HttpClient.GetAsync($"https://api.steampowered.com/IStoreBrowseService/GetItems/v1/?input_json={payloadUrlEncoded}", System.Net.Http.HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false))
+            var jsonPayload = JsonSerializer.Serialize(getItemsInput);
+            var payloadUrlEncoded = HttpUtility.UrlEncode(jsonPayload);
+
+            using (var steamApiResponse = await App.CurrentApp.HttpClient.GetAsync($"https://api.steampowered.com/IStoreBrowseService/GetItems/v1/?input_json={payloadUrlEncoded}", System.Net.Http.HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false))
+            {
+                if (steamApiResponse.IsSuccessStatusCode == false)
                 {
-                    if (steamApiResponse.IsSuccessStatusCode == false)
-                    {
-                        Logger.Error($"Failed to load Steam cover for {PlatformId} from IStoreBrowseService. Status code: {steamApiResponse.StatusCode}");
-                        return;
-                    }
+                    Logger.Error($"Failed to load Steam cover for {PlatformId} from IStoreBrowseService. Status code: {steamApiResponse.StatusCode}");
+                    return false;
+                }
 
-                    using (var responseStream = await steamApiResponse.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                using (var responseStream = await steamApiResponse.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                {
+                    var response = JsonSerializer.Deserialize<SteamAPIResponse<GetItemsResponse>>(responseStream, SourceGenerationContext.Default.SteamAPIResponseGetItemsResponse);
+                    if (response?.Response?.StoreItems.Any() == true)
                     {
-                        var response = JsonSerializer.Deserialize<SteamAPIResponse<GetItemsResponse>>(responseStream, SourceGenerationContext.Default.SteamAPIResponseGetItemsResponse);
-                        if (response?.Response?.StoreItems.Any() == true)
+                        // We are only doing one search, so we likely only care for the first item.
+                        var storeItem = response.Response.StoreItems[0];
+
+                        if (storeItem.Assets is null)
                         {
-                            // We are only doing one search, so we likely only care for the first item.
-                            var storeItem = response.Response.StoreItems[0];
+                            Logger.Error($"No Assets found for {PlatformId} in the response from IStoreBrowseService.");
+                            return false;
+                        }
 
-                            if (storeItem.Assets is null)
-                            {
-                                Logger.Error($"No Assets found for {PlatformId} in the response from IStoreBrowseService.");
-                                return;
-                            }
+                        if (string.IsNullOrWhiteSpace(storeItem.Assets.AssetUrlFormat))
+                        {
+                            Logger.Error($"No AssetUrlFormat found for {PlatformId} in the response from IStoreBrowseService.");
+                            return false;
+                        }
 
-                            if (string.IsNullOrWhiteSpace(storeItem.Assets.AssetUrlFormat))
+                        // We are only checking LibraryCapsule2x, hopefully it exists for all games
+                        if (string.IsNullOrWhiteSpace(storeItem.Assets.LibraryCapsule2x) == false)
+                        {
+                            // There are 3 different CDNs, I don't lknow what one they will use, so lets try all of them?
+                            var cdns = new[]
                             {
-                                Logger.Error($"No AssetUrlFormat found for {PlatformId} in the response from IStoreBrowseService.");
-                                return;
-                            }
-
-                            // We are only checking LibraryCapsule2x, hopefully it exists for all games
-                            if (string.IsNullOrWhiteSpace(storeItem.Assets.LibraryCapsule2x) == false)
-                            {
-                                // There are 3 different CDNs, I don't lknow what one they will use, so lets try all of them?
-                                var cdns = new[]
-                                {
                                     "https://shared.fastly.steamstatic.com",
                                     "https://shared.steamstatic.com",
                                     "https://shared.akamai.steamstatic.com"
                                 };
 
-                                foreach (var cdn in cdns)
+                            foreach (var cdn in cdns)
+                            {
+                                var coverUrl = $"{cdn}/store_item_assets/{storeItem.Assets.AssetUrlFormat.Replace("${FILENAME}", storeItem.Assets.LibraryCapsule2x)}";
+                                var didDownloadCover = await DownloadCoverAsync(coverUrl).ConfigureAwait(false);
+                                if (didDownloadCover)
                                 {
-                                    var coverUrl = $"{cdn}/store_item_assets/{storeItem.Assets.AssetUrlFormat.Replace("${FILENAME}", storeItem.Assets.LibraryCapsule2x)}";
-                                    var didDownloadCover = await DownloadCoverAsync(coverUrl).ConfigureAwait(false);
-                                    if (didDownloadCover)
-                                    {
-                                        return;
-                                    }
-                                    Logger.Error($"Could not download cover \"{storeItem.Assets.LibraryCapsule2x}\" with CDN {cdn} so trying next.");
+                                    return true;
                                 }
+                                Logger.Error($"Could not download cover \"{storeItem.Assets.LibraryCapsule2x}\" with CDN {cdn} so trying next.");
                             }
                         }
-                        else
-                        {
-                            Logger.Error($"No store items found for {PlatformId} in the response from IStoreBrowseService.");
-                        }
+                    }
+                    else
+                    {
+                        Logger.Error($"No store items found for {PlatformId} in the response from IStoreBrowseService.");
                     }
                 }
+            }
 
-                Logger.Error($"Tried all known methods to get Steam cover for {PlatformId}, but all had failed.");
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex, $"Failed to load Steam cover for {PlatformId} from IStoreBrowseService.");
-            }
+            Logger.Error($"Tried all known methods to get Steam cover for {PlatformId}, but all had failed.");
         }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, $"Failed to load Steam cover for {PlatformId} from IStoreBrowseService.");
+        }
+
+        return false;
     }
 
     public override bool UpdateFromGame(Game game)

b75492264d2dd031d682f8a257ca01270bedb058
Brad <brad@4pi.com.au>
2025-12-21 17:16:35 +1100

    Updated the static manifest.

--- extras/update_manifest.cmd
+++ extras/update_manifest.cmd
@@ -1,2 +1,3 @@
 set current_directory=%~dp0
 curl.exe --output "%current_directory%..\src\Assets\static_manifest.json" --url https://raw.githubusercontent.com/beeradmoore/dlss-swapper-manifest-builder/refs/heads/main/manifest.json
+copy "%current_directory%..\src\Assets\static_manifest.json" "%current_directory%..\docs\manifest.json"

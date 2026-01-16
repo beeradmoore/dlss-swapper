09bfaa388cbab84cf8f416576b6e401d64f98e10
Brad <brad@4pi.com.au>
2026-01-16 20:11:07 +1100

    Fixes an issue where closing a window while minimized won't yeet it to -32000 x/y pos (#830)

--- src/Data/WindowPositionRect.cs
+++ src/Data/WindowPositionRect.cs
@@ -35,10 +35,34 @@ public class WindowPositionRect
         Width = other.Width;
         Height = other.Height;
         State = other.State;
+
+        // -32000 is some magic number were windows go to die.
+        if (X == -32000)
+        {
+            X = 0;
+        }
+
+        if (Y == -32000)
+        {
+            Y = 0;
+        }
+
     }
 
     public WindowPositionRect(int x, int y, int width, int height)
     {
+        // -32000 is some magic number were windows go to die.
+        // This is to help apps that are already broken to show the main window again.
+        if (x == -32000)
+        {
+            x = 0;
+        }
+
+        if (y == -32000)
+        {
+            y = 0;
+        }
+
         X = x;
         Y = y;
         Width = width;
@@ -47,17 +71,41 @@ public class WindowPositionRect
 
     public RectInt32 GetRectInt32()
     {
+        // -32000 is some magic number were windows go to die.
+        // This is to help apps that are already broken to show the main window again.
+        if (X == -32000)
+        {
+            X = 0;
+        }
+
+        if (Y == -32000)
+        {
+            Y = 0;
+        }
+
         return new RectInt32(X, Y, Width, Height);
     }
 
     public void UpdatePosition(PointInt32 position)
     {
+        // -32000 is some magic number were windows go to die.
+        if (position.X == -32000 || position.Y == -32000)
+        {
+            return;
+        }
+
         X = position.X;
         Y = position.Y;
     }
 
     public void UpdateFromAppWindow(AppWindow appWindow)
     {
+        // -32000 is some magic number were windows go to die.
+        if (appWindow.Position.X == -32000 || appWindow.Position.Y == -32000)
+        {
+            return;
+        }
+
         Width = appWindow.Size.Width;
         Height = appWindow.Size.Height;
         X = appWindow.Position.X;

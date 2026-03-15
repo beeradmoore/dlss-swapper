2e2304eb45901a909d087ca4f891c37fe1187b03
Brad <brad@4pi.com.au>
2026-03-15 11:45:48 +1100

    Added support for getting SHA256 hash of a file

--- src/Extensions/FileStreamExtensions.cs
+++ src/Extensions/FileStreamExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace DLSS_Swapper.Extensions;
 
@@ -9,9 +10,20 @@ internal static class FileStreamExtensions
     {
         fileStream.Position = 0;
 
-        using (var md5 = System.Security.Cryptography.MD5.Create())
+        using (var md5 = MD5.Create())
         {
             var hash = md5.ComputeHash(fileStream);
+            return BitConverter.ToString(hash).Replace("-", "").ToUpperInvariant();
+        }
+    }
+
+    internal static string GetSha256Hash(this Stream fileStream)
+    {
+        fileStream.Position = 0;
+
+        using (var sha256 = SHA256.Create())
+        {
+            var hash = sha256.ComputeHash(fileStream);
             return BitConverter.ToString(hash).Replace("-", "").ToUpperInvariant();
         }
     }
